6a52be941f64f461eb2404c55cb34d84dac1b88b
kevinli296 <kevinli296@outlook.com>
2020-12-10 22:21:31 -0800

    Implement getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,3 @@
-// function selectUserByName(state, name) {
-//   const filteredNames = state.users.filter(user => user.name === name);
-//   return filteredNames;
-// }
-
 export function getAppointmentsForDay(state, day) {
   //... returns an array of appointments for that day
   const result = [];
@@ -17,27 +12,23 @@ export function getAppointmentsForDay(state, day) {
     }
   })
   
-  // console.log(selectedDay);
-
-  // for (const tmp of selectedDay.appointments) {
-  //   appointments.push(tmp);
-  // }
-
-  // if (selectedDay.length !==)
-  // for (const tmp of selectedDay) {
-  //   for (const tmp2 in state.appointments) {
-  //     if (state.appointments[tmp2].id === tmp) {
-  //       result.push(state.appointments[tmp2]);
-  //     }
-  //   }
-  // }
-  
   return result;
 
 }
 
-export function getInterviewersForDay() {
-  
+export function getInterviewersForDay(state, day) {
+  const result = [];
+
+  state.days.filter(tmpDay => {
+    if (tmpDay.name === day) {
+      for (const interview of tmpDay.interviewers) {
+        result.push(state.interviewers[interview]);
+      }
+    }
+  })
+
+  return result;
+
 }
 
 export function getInterview(state, interview) {
