7fc2bbe8b51d8ae8f3b37e097c0f3f806a6257cf
kevinli296 <kevinli296@outlook.com>
2020-12-10 22:34:11 -0800

    Adjusted Form component to render interviewers

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import { useVisualMode } from "../../hooks/useVisualMode";
 import Form from "components/Appointment/Form";
+import { getInterviewersForDay } from "../../helpers/selectors"
 
 import "components/Appointment/styles.scss";
 
@@ -20,17 +21,17 @@ export default function Appointment(props) {
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => mode === transition(CREATE)} />}
-      {mode === CREATE && (
-        <Form
-          interviewers={[]}
-          onCancel={() => mode === back()}
-          />
-      )}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
+      )}
+            {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          onCancel={() => mode === back()}
+          />
       )}
       {
         // props.interview ?
