9ade2daaf16cace8658c5b6455bfc82abac79b73
kevinli296 <kevinli296@outlook.com>
2020-12-11 23:07:48 -0800

    Code cleanup

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -16,6 +16,10 @@ export default function Form(props) {
     props.onCancel()
   }
 
+  const saving = function() {
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
@@ -42,7 +46,7 @@ export default function Form(props) {
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button onClick={cancel} danger>Cancel</Button>
-      <Button onClick={props.onSave} confirm>Save</Button>
+      <Button onClick={saving} confirm>Save</Button>
     </section>
   </section>
 </main>
