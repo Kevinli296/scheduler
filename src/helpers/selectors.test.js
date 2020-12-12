6fb870a147e9a9bd7a68fce26b377a5f4abea3b9
kevinli296 <kevinli296@outlook.com>
2020-12-11 23:07:23 -0800

    Code cleanup

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -17,8 +17,7 @@ const state = {
   ],
   appointments: {
     "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm",
-      interview: null },
+    "2": { id: 2, time: "1pm", interview: null },
     "3": {
       id: 3,
       time: "2pm",
