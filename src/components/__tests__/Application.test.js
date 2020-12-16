9f3a87f9c665b517e60caa7c4acc3f7c5918a5e3
kevinli296 <kevinli296@outlook.com>
2020-12-16 01:39:49 -0800

    Add tests for error handling

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,6 +3,7 @@ import React from "react";
 import { render, cleanup, waitForElement, fireEvent, getByText, getByAltText, getByPlaceholderText, getAllByTestId, queryByText, queryByAltText, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
+import axios from "axios";
 
 afterEach(cleanup);
 
@@ -63,7 +64,9 @@ it("loads data, books an interview and reduces the spots remaining for the first
   const day = getAllByTestId(container, "day").find(day =>
     queryByText(day, "Monday")
   );
+
   expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
+
 });
 
 it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
@@ -79,14 +82,19 @@ it("loads data, cancels an interview and increases the spots remaining for Monda
 
   // 3. Click the "Delete" button on the booked appointment.
   fireEvent.click(queryByAltText(appointment, "Delete"));
+
   // 4. Check that the element with the confirmation message is shown.
   expect(getByText(appointment, /Are you sure you would like to delete?/i));
+
   // 5. Click the "Confirm" button on the confirmation.
   fireEvent.click(queryByText(appointment, "Confirm"));
+
   // 6. Check that the element with the text "Deleting" is displayed.
   expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
   // 7. Wait until the element with the "Add" button is displayed.
   await waitForElement(() => getByAltText(appointment, "Add"));
+
   // 8. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining" (Because the previous test decreases the spots by 1).
   const day = getAllByTestId(container, "day").find(day =>
     queryByText(day, "Monday")
@@ -108,28 +116,97 @@ it("loads data, edits an interview and keeps the spots remaining for Monday the 
 
   // 3. Click the "Edit" button on the booked appointment.
   fireEvent.click(queryByAltText(appointment, "Edit"));
+
   // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i),
   {target: {value: "Lydia Miller-Jones"}});
+
   // 5. Click the first interviewer in the list.
   fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
   // 6. Click the "Save" button on that same appointment.
   fireEvent.click(getByText(appointment, "Save"));
+
   // 7. Check that the element with the text "Saving" is displayed.
   expect(getByText(appointment, "Saving"));
+
   // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
   await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
   // 9. Check that the DayListItem with the text "Monday" also has the same amount of spots remaining.
   const day = getAllByTestId(container, "day").find(day =>
     queryByText(day, "Monday")
   );
 
   expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
+
+});
+
+it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce();
+    // 1. Render the Application.
+    const { container, debug } = render(<Application/>)
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
+    // 3. Click the "Add" button on the first empty appointment.
+    fireEvent.click(getByAltText(appointment, "Add"));
+  
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i),
+    {target: { value: "Lydia Miller-Jones"}});
+  
+    // 5. Click the first interviewer in the list.
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    // 6. Click the "Save" button on that same appointment.
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // 7. Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    
+    // 8. Wait until the element with the error message is shown.
+    await waitForElement(() => expect(getByText(appointment, /Could not save appointment./i)));
+
+    // 9. Check that the element with the error message is displayed.
+    expect(getByText(appointment, /Could not save appointment./i)).toBeInTheDocument();
+
+});
+
+it("shows the delete error when failing to delete an appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+  
+    // 3. Click the "Delete" button on the booked appointment.
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+  
+    // 4. Check that the element with the confirmation message is shown.
+    expect(getByText(appointment, /Are you sure you would like to delete?/i));
+  
+    // 5. Click the "Confirm" button on the confirmation.
+    fireEvent.click(queryByText(appointment, "Confirm"));
+
+    // 6. Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+    // 7. Wait until the element with the error message is displayed.
+    await waitForElement(() => expect(getByText(appointment, /Could not delete appointment./i)));
+
+    // 8. Check that the element with the error message is displayed.
+    expect(getByText(appointment, /Could not delete appointment./i)).toBeInTheDocument();
+    
 });
 
 })
-
-// "loads data, cancels an interview and increases the spots remaining for Monday by 1"
-// "loads data, edits an interview and keeps the spots remaining for Monday the same"
-// "shows the save error when failing to save an appointment"
-// "shows the delete error when failing to delete an existing appointment"
