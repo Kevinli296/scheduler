43c93759a08e8288f85fae254a2c07524ffcc4dd
kevinli296 <kevinli296@outlook.com>
2020-12-15 21:29:05 -0800

    Add test for editing an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -95,6 +95,38 @@ it("loads data, cancels an interview and increases the spots remaining for Monda
   expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
 });
 
+it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+  // 1. Render the Application.
+  const { container, debug } = render(<Application />);
+
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  const appointment = getAllByTestId(container, "appointment").find(
+    appointment => queryByText(appointment, "Archie Cohen")
+  );
+
+  // 3. Click the "Edit" button on the booked appointment.
+  fireEvent.click(queryByAltText(appointment, "Edit"));
+  // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i),
+  {target: {value: "Lydia Miller-Jones"}});
+  // 5. Click the first interviewer in the list.
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  // 6. Click the "Save" button on that same appointment.
+  fireEvent.click(getByText(appointment, "Save"));
+  // 7. Check that the element with the text "Saving" is displayed.
+  expect(getByText(appointment, "Saving"));
+  // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
+  await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  // 9. Check that the DayListItem with the text "Monday" also has the same amount of spots remaining.
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+
+  expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
+});
+
 })
 
 // "loads data, cancels an interview and increases the spots remaining for Monday by 1"
