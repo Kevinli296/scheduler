ee5fa75c242fb85e9889ff9d941eee88245453c2
kevinli296 <kevinli296@outlook.com>
2020-12-15 17:57:27 -0800

    Add test case to application utilizing mock put function

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, getByAltText, getByPlaceholderText, getAllByTestId, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -11,7 +11,7 @@ xit("renders without crashing", () => {
   render(<Application />);
 });
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
   // return waitForElement(() => getByText("Monday")) // Promise Chaining
@@ -27,5 +27,36 @@ it("defaults to Monday and changes the schedule when a new day is selected", () 
   expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
 
+it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  const { container } = render(<Application/>)
+
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  // console.log(prettyDOM(container));
+  const appointments = getAllByTestId(container, "appointment");
+  // console.log(prettyDOM(appointments));
+  const appointment = appointments[0];
+  // console.log(prettyDOM(appointment));
+
+  fireEvent.click(getByAltText(appointment, "Add"));
+
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i),
+  {target: { value: "Lydia-Miller Jones"}});
+
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  fireEvent.click(getByText(appointment, "Save"));
 
 })
+
+})
+
+// Render the Application.
+// Wait until the text "Archie Cohen" is displayed.
+// Click the "Add" button on the first empty appointment.
+// Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+// Click the first interviewer in the list.
+// Click the "Save" button on that same appointment.
+// Check that the element with the text "Saving" is displayed.
+// Wait until the element with the text "Lydia Miller-Jones" is displayed.
+// Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
